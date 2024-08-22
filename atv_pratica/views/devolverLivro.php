9fffc9ccbfb9dfeda0b25e20accf7723b2a53243
Geovanna <geovanna.lima60@aluno.ifce.edu.br>
2024-08-21 22:33:33 -0300

    Formulário devolverLivro

--- atv_pratica/views/devolverLivro.php
+++ atv_pratica/views/devolverLivro.php
@@ -30,29 +30,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../views/css/style.css">
     <title>Devolução de Livros</title>
     <?php include 'menu.php'; ?>
 </head>
 <body>
-    <h1>Cadastrar Nova Devolução</h1>
-    
-    <a href="index.php">Voltar para a página inicial</a>
-    <form action="devolverLivro.php" method="post">
-        <label for="livro">Escolha um Livro:</label>
-            <select name="livro" id="livro" required>
-                <?php foreach ($livros as $livro): ?>
-                    <option value="<?php echo $livro->getTitulo(); ?>">
-                        <?php echo $livro->getTitulo(); ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-        <br>
-        <label for="data_devolucao">Data de Devolução:</label>
-        <input type="date" id="data_devolucao" name="data_devolucao" required>
-        <br>
-        
-        <input type="submit" value="Cadastrar Devolução">
-    </form>
+    <div class="container">
+        <section>
+            <div>
+                <h1 class="titulo-form-devolverLivro">Cadastrar Nova Devolução</h1>
+            </div>
+            <div>
+                <img src="../views/imgs/" alt="">
+            </div>
+        </section>
+
+        <div>
+            <form action="devolverLivro.php" method="post">
+                <label class="form-group" for="livro">ESCOLHA UM LIVRO:</label>
+                    <select name="livro" id="livro" required>
+                        <?php foreach ($livros as $livro): ?>
+                            <option value="<?php echo $livro->getTitulo(); ?>">
+                                <?php echo $livro->getTitulo(); ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                <br>
+                <label class="form-group" for="data_devolucao">DATA DE DEVOLUÇÃO:</label>
+                <input type="date" id="data_devolucao" name="data_devolucao" required>
+                <br>
+                
+                <input type="submit" value="Cadastrar Devolução">
+            </form>
+        </div>
+    </div>
+
     <?php if (isset($controller_livro)) : ?>
         <div class="mensagem"><?php echo $controller_biblioteca->getMensage(); ?></div>
     <?php endif; ?>
