fd10f25dbf6dcdfcb32feb835a4688e4fcd1266b
Geovanna <geovanna.lima60@aluno.ifce.edu.br>
2024-08-21 22:42:59 -0300

    Update editarEstudante.php

--- atv_pratica/views/editarEstudante.php
+++ atv_pratica/views/editarEstudante.php
@@ -28,21 +28,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../views/css/style.css">
     <title>Editar Estudante</title>
     <?php include 'menu.php'; ?>
 </head>
 <body>
-    <h1>Editar Estudante</h1>
-    <a href="listarEstudantes.php">Voltar para a lista</a>
-    <form action=<?php echo "'editarEstudante.php?nome_antigo=".$estudante->getNome()."'"?> method="post">
-        <label for="nome_antigo">Nome Antigo:</label>
-        <input type="text" id="nome_antigo" disabled name="nome_antigo" value="<?php echo htmlspecialchars($estudante->getNome()); ?>" required>    
-        <br>
-        <label for="nome">Novo Nome:</label>
-        <input type="text" id="nome" name="nome" required>
-        <br>
-        <input type="submit" value="Atualizar">
-    </form>
+<div class="container">
+        <section>
+            <div>
+                <h1>Editar Estudante</h1>
+            </div>
+            <div>
+                <img src="../views/imgs/book-open-svgrepo-com (1).svg" alt="">
+            </div>
+        </section>
+
+        <div>
+            <form action=<?php echo "'editarEstudante.php?nome_antigo=".$estudante->getNome()."'"?> method="post">
+                <label for="nome_antigo">NOME ANTIGO:</label>
+                <input type="text" id="nome_antigo" disabled name="nome_antigo" value="<?php echo htmlspecialchars($estudante->getNome()); ?>" required>    
+                <br>
+                <label for="nome">NOVO NOME:</label>
+                <input type="text" id="nome" name="nome" required>
+                <br>
+                <input type="submit" value="Atualizar">
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
